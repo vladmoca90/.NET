a6e2db36cfe103e05509d546439444963fb2c180
Vlad Mocanu <vladmoca90@gmail.com>
2026-06-23 14:04:18 +0100

    Deleted the old project completely

--- APIs/obj/Debug/net10.0/APIs.AssemblyInfo.cs
+++ APIs/obj/Debug/net10.0/APIs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88055182348a294c274b65662fbf4aeda42fcf20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2808877d23271829621717c4968f0c65788fd7ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

f2e6d1f4b9481f019fc0c3c40d8153c8a17a7d3f
Vlad Mocanu <vladmoca90@gmail.com>
2026-07-16 23:56:10 +0100

    Working on the CarShowroom project

--- CarShowroom/obj/Debug/net10.0/CarShowroom.AssemblyInfo.cs
+++ CarShowroom/obj/Debug/net10.0/CarShowroom.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarShowroom")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab600b4158e5193a5231bb0a3c293c2a3e80010")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda93f9d910489a039e5224d783b570297afb599")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarShowroom")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarShowroom")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
